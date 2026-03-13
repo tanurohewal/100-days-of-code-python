dccf363d7043273d04490169ce0d645230ac774d
Tanu Rohewal <tanurohewal@gmail.com>
2026-03-14 00:57:33 +0530

    Add Day-17-Quiz Brain Game

---
+++ Day17_QuizBrain/README.md
@@ -0,0 +1,26 @@
+🧠 Day 17: Quiz Game using OOP
+
+📄 Project Overview  
+This project is a **Quiz Game** built using **Object-Oriented Programming (OOP)** in Python as part of my **100 Days of Code – Python** journey.  
+The program asks True/False questions, checks the user's answers, and keeps track of the score.
+
+🎯 Objective  
+- Understand Object-Oriented Programming concepts  
+- Learn how to create and use classes  
+- Organize code using multiple Python files  
+- Build an interactive command-line quiz application  
+
+✨ Features  
+- Multiple True/False questions  
+- Score tracking system  
+- Instant feedback (Correct / Wrong)  
+- Displays the correct answer  
+- Shows final score at the end of the quiz  
+
+🛠 Concepts Used  
+- Classes and Objects  
+- Constructors (`__init__`)  
+- Lists and Dictionaries  
+- Loops  
+- If-Else Conditions  
+- Modular Programming
---
+++ Day17_QuizBrain/data.py
@@ -0,0 +1,62 @@
+question_data = [
+    {
+
+      "question": "The Sun rises from the North.",
+      "correct_answer": "False"
+    },
+    {
+
+      "question": "A scientific study on peanuts in bars found traces of over 100 unique specimens of urine.",
+      "correct_answer": "False"
+    },
+    {
+
+      "question": "Slovakia is a member of European Union-",
+      "correct_answer": "True"
+    },
+    {
+
+      "question": "Studies suggest that approximately 40% of the world population is left-handed.",
+      "correct_answer": "False"
+    },
+    {
+
+      "question": "Dihydrogen Monoxide is a dangerous chemical.",
+      "correct_answer": "False"
+    },
+    {
+
+      "question": "All strawberries are red.",
+      "correct_answer": "False"
+    },
+    {
+
+      "question": "French is an official language in Canada.",
+      "correct_answer": "True"
+    },
+    {
+
+      "question": "Is a dog a mammal?",
+      "correct_answer": "True"
+    },
+    {
+
+      "question": "Water is naturally coloured blue.",
+      "correct_answer": "False"
+    },
+    {
+
+      "question": "Did Michael Jackson invent the moonwalk?",
+      "correct_answer": "False"
+    },
+    {
+
+      "question": "Scotland voted to become an independent country during the referendum from September 2014.",
+      "correct_answer": "False"
+    },
+    {
+
+      "question": "March 10th is also known as Mar10 Day.",
+      "correct_answer": "True"
+    }
+  ]
